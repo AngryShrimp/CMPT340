498622e6caf29268fdf87258ddfd8e274192a114
AngryShrimp <johnstone.keenan@gmail.com>
2017-02-13 16:24:57 -0600

    Problems 1-5 done, 6 half done

--- Assignment2/programs.hs
+++ Assignment2/programs.hs
@@ -29,10 +29,11 @@ test2 :: MyFloat
 test2 = MyFloat(670, 2)
 
 test3 :: MyFloat
-test3 = MyFloat(400 , 3)
+test3 = MyFloat(400, 3)
 
 instance Show MyFloat where
-    show (MyFloat (m, e)) = "0." ++ (show m) ++ " * 10^" ++ (show e)
+    show (MyFloat (m, e)) = if (m >= 0) then "0." ++ (show m) ++ " * 10^" ++ (show e)
+                            else             "-0." ++ (show (-m)) ++ " * 10^" ++ (show e)
 
 instance Eq MyFloat where
     (==) = eqMyFloat
@@ -42,11 +43,66 @@ eqMyFloat (MyFloat(m1, e1)) (MyFloat(m2, e2)) = ((m1*(10^e1)) == (m2*(10^e2)))
 
 instance Num MyFloat where
     (+) = addMyFloat
+    (-) = subMyFloat
+    (*) = mulMyFloat
+    negate = negMyFloat
 
 addMyFloat :: MyFloat -> MyFloat -> MyFloat
-addMyFloat (MyFloat(m1, e1)) (MyFloat(m2, e2)) = if (e1 == e2) then MyFloat((m1+m2), e1)
+addMyFloat (MyFloat(m1, e1)) (MyFloat(m2, e2)) = if (e1 == e2)     then MyFloat((m1+m2), e1)
                                                  else if (e1 > e2) then MyFloat(((m1*10^(e1-e2))+m2), e1)
-                                                 else MyFloat(((m2*10^(e2-e1))+m1), e2)
+                                                 else                   MyFloat(((m2*10^(e2-e1))+m1), e2)
+
+subMyFloat :: MyFloat -> MyFloat -> MyFloat
+subMyFloat (MyFloat(m1, e1)) (MyFloat(m2, e2)) = if (e1 == e2)     then MyFloat((m1-m2), e1)
+                                                 else if (e1 > e2) then MyFloat(((m1*10^(e1-e2))+m2), e1)
+                                                 else                   MyFloat(((m2*10^(e2-e1))+m1), e2)
+
+mulMyFloat :: MyFloat -> MyFloat -> MyFloat
+mulMyFloat (MyFloat(m1, e1)) (MyFloat(m2, e2)) = MyFloat((m1*m2), (e1+e2))
+
+negMyFloat :: MyFloat -> MyFloat
+negMyFloat (MyFloat(m, e)) = MyFloat((-m), e)
+
+instance Fractional MyFloat where
+    (/) = divMyFloat 
+
+divMyFloat :: MyFloat -> MyFloat -> MyFloat
+divMyFloat (MyFloat(m1, e1)) (MyFloat(m2, e2)) = MyFloat((m1`div`m2), (e1-e2+1))
+
+instance Ord MyFloat where
+    (<=) = lteMyFloat
+    (<)  = ltMyFloat
+    (>=) = gteMyFloat
+    (>)  = gtMyFloat
+
+lteMyFloat :: MyFloat -> MyFloat -> Bool
+lteMyFloat (MyFloat(m1, e1)) (MyFloat(m2, e2)) = (m1 <= m2 && e1 <= e2) 
+
+ltMyFloat :: MyFloat -> MyFloat -> Bool
+ltMyFloat (MyFloat(m1, e1)) (MyFloat(m2, e2)) = if (e1 < e2)       then True
+                                                else if (e1 == e2) then (m1 < m2)
+                                                else                    False
+
+gteMyFloat :: MyFloat -> MyFloat -> Bool
+gteMyFloat (MyFloat(m1, e1)) (MyFloat(m2, e2)) = (m1 >= m2 && e1 >= e2) 
+
+gtMyFloat :: MyFloat -> MyFloat -> Bool
+gtMyFloat (MyFloat(m1, e1)) (MyFloat(m2, e2)) = if (e1 > e2)       then True
+                                                else if (e1 == e2) then (m1 > m2)
+                                                else                    False
+
+whole :: MyFloat -> Integer
+whole (MyFloat(m,e)) = m `div` 10^((digitCount m)-e)
+
+fraction :: MyFloat -> Float
+fraction (MyFloat(m,e)) = (fromInteger (m `mod` 10^((digitCount m)-e))) * (10^^(negate (digitCount m) + e))
+
+
+--For counting the number of digits in a number
+digitCount :: Integer -> Integer
+digitCount = go 1 . abs
+    where
+        go ds n = if n >= 10 then go (ds + 1) (n `div` 10) else ds
 
 --Problem 4
 
